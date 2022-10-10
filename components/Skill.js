e30485758335a732589c34930a1a535dead55496
Nicolas Regenhardt <nrx.dev@gmail.com>
2022-10-10 20:32:55 -0300

    remove-skill-effect

--- components/Skill.js
+++ components/Skill.js
@@ -5,11 +5,10 @@ const Skill = ({ directionLeft }) => {
     <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{
-          x: directionLeft ? -100 : 100,
           opacity: 0,
         }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
         src="https://1000logos.net/wp-content/uploads/2017/07/Dell-Logo.png"
         className="rounded-full border border-gray-600 object-cover h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
